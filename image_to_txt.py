15e1c01ba6f367154ea496ed5ea070aa07585307
Behrad Tondi <112078003+behrad26@users.noreply.github.com>
2023-09-20 09:23:33 +0330

    Update image_to_txt.py

--- image_to_txt.py
+++ image_to_txt.py
@@ -1,18 +1,15 @@
-import cv2
-image, txt = input("Image: "), input("Destination TXT: ")
+import sys
+from cv2 import imread
+from math import ceil
+lst = ["M", "@", "Q", "&", "0", "#", "$", "%", "A", "+", "-", ".", " "]
+image, txt, chars = input("Image: "), input("Destination TXT: "), "".join([i * ceil(255 / len(lst)) for i in lst])
 open(txt, "w").close()
 with open(txt, "a") as file:
-    for row in cv2.imread(image):
+    img = imread(image)
+    if not img:
+        print(f"Couldn't read {image}")
+        sys.exit(1)
+    for row in img:
         for pixel in row:
-            n = sum(pixel) // 3
-            if 0 <= n < 26: file.write("M ")
-            elif 26 <= n < 51: file.write("# ")
-            elif 51 <= n < 76: file.write("Q ")
-            elif 76 <= n < 102: file.write("A ")
-            elif 102 <= n < 128: file.write("& ")
-            elif 128 <= n < 153: file.write("0 ")
-            elif 153 <= n < 178: file.write("+ ")
-            elif 178 <= n < 204: file.write("- ")
-            elif 204 <= n < 230: file.write(". ")
-            elif 230 <= n < 256: file.write("  ")
+            file.write(chars[ceil(sum(pixel) / 3)])
         file.write("\n")
