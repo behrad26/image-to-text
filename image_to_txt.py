03b2e7148aaff05a193a3e5975726a54ca45635f
Behrad Tondi <112078003+behrad26@users.noreply.github.com>
2023-09-20 09:27:19 +0330

    Update image_to_txt.py

--- image_to_txt.py
+++ image_to_txt.py
@@ -5,11 +5,7 @@ lst = ["M", "@", "Q", "&", "0", "#", "$", "%", "A", "+", "-", ".", " "]
 image, txt, chars = input("Image: "), input("Destination TXT: "), "".join([i * ceil(255 / len(lst)) for i in lst])
 open(txt, "w").close()
 with open(txt, "a") as file:
-    img = imread(image)
-    if not img:
-        print(f"Couldn't read {image}")
-        sys.exit(1)
-    for row in img:
+    for row in imread(image):
         for pixel in row:
             file.write(chars[ceil(sum(pixel) / 3)])
         file.write("\n")
